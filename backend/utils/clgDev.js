21487227b05a099a7aafd2ea272058b47ada8516
Anik Roy <71890060+Eternatus29@users.noreply.github.com>
2026-04-09 01:00:00 +0530

    chore: initialize project from Next.js starter

--- backend/utils/clgDev.js
+++
@@ -1,7 +0,0 @@
-const clgDev = (x) => {
-  if (process.env.NODE_ENV === 'development') {
-    console.log(x);
-  }
-}
-
-module.exports = clgDev;
--- frontend/src/utils/dateFormatter.js
+++
@@ -1,9 +0,0 @@
-const dateFormatter = (date) => {
-  return new Date(date).toLocaleDateString('en-us', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric'
-  })
-}
-
-export default dateFormatter
