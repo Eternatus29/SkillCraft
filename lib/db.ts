64cc3022ebbd657136b5023c9b181bc0c2f0c9f4
Anik Roy <developer.anikroy@gmail.com>
2026-04-24 15:20:00 +0530

    refactor: centralize Mongoose model registration in one bootstrap point

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import '@/models/register'
 
 const MONGODB_URI = process.env.MONGODB_URI!
 
---
+++ models/register.ts
@@ -0,0 +1,12 @@
+import '@/models/Profile'
+import '@/models/User'
+import '@/models/Category'
+import '@/models/SubSection'
+import '@/models/Section'
+import '@/models/Course'
+import '@/models/Review'
+import '@/models/CourseProgress'
+import '@/models/OTP'
+
+// Importing this module ensures all schemas are registered before populate queries run.
+export {}
